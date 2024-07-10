f7844b8ae040d8965fcd1216181948ebd70ca914
Marcel Slowikowski <marceljs041@gmail.com>
2024-07-09 20:59:05 -0500

    Update withAuth.tsx

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -2,7 +2,7 @@ import { useEffect, ComponentType } from 'react';
 import { useRouter } from 'next/navigation';
 
 const withAuth = <P extends object>(WrappedComponent: ComponentType<P>) => {
-  return (props: P) => {
+  const ComponentWithAuth = (props: P) => {
     const router = useRouter();
 
     useEffect(() => {
@@ -14,6 +14,10 @@ const withAuth = <P extends object>(WrappedComponent: ComponentType<P>) => {
 
     return <WrappedComponent {...props} />;
   };
+
+  ComponentWithAuth.displayName = `WithAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return ComponentWithAuth;
 };
 
 export default withAuth;
